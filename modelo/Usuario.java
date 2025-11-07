71d08fbe0e1e5e3b16d7e576521d0d27944e6ec6
knight2603 <sierrasebastian206@gmail.com>
2025-11-07 15:17:53 -0500

    Add Usuario class with getters and setters

--- modelo/Usuario.java
+++ modelo/Usuario.java
@@ -1,1 +1,66 @@
 
+package modelo;
+
+public class Usuario {
+    
+    private int cc;
+    private String nombre, lastname, correo, pswd, tipo, foto;
+
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPswd() {
+        return pswd;
+    }
+
+    public void setPswd(String pswd) {
+        this.pswd = pswd;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+    
+}
+
