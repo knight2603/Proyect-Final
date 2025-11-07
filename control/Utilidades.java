c7be584f8de385ea3c7da447605ccceba2a46cad
knight2603 <sierrasebastian206@gmail.com>
2025-11-07 15:47:25 -0500

    Add Utilidades class with MD5 encryption method

---
+++ control/Utilidades.java
@@ -0,0 +1,25 @@
+package control;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Utilidades {
+
+    public static String encriptar(String cad) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(cad.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
